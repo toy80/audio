9093e85b59d696d667ad3623d16886780489005e
toy80 <12060052+toy80@users.noreply.github.com>
2019-08-04 14:09:29 +0800

    fix typo

--- vorbis/vorbis.go
+++ vorbis/vorbis.go
@@ -1,5 +1,5 @@
 // Package vorbis is a vorbis decoder implementation in pure golang.
-// it was write from scratch follow the [Vorbis I specification](https://xiph.org/vorbis/doc/Vorbis_I_spec.html).
+// it was wrote from scratch follow the [Vorbis I specification](https://xiph.org/vorbis/doc/Vorbis_I_spec.html).
 package vorbis
 
 import (
